726360308c925435db6e845fae71dddaf6162a97
Mariusz Nowak <medyk@medikoo.com>
2017-06-01 16:07:10 +0200

    💥

---
+++ README.md
@@ -0,0 +1,32 @@
+# date-from-timezone
+## Construct dates with reference to timezone context
+
+Having timezone and date & time information, returns regular date instance that corresponds do desired moment in time.
+
+Light implementation that stands on supported in modern engines [Intl.DateTimeFormat](http://www.ecma-international.org/ecma-402/1.0/#sec-12.1) interface.
+
+_In environments that do not provide `Intl.DateTimeFormat` or of which support is incomplete module resolves to `null`_
+
+### Example
+
+```javascript
+var DateFromTimezone = require("./");
+
+var dateFromWarsaw = new DateFromTimezone("Europe/Warsaw");
+var warsawNoon = dateFromWarsaw.getDate(2017, 6, 5, 12);
+console.log(warsawNoon.toISOString()); // "2017-07-05T10:00:00.000Z" (12PM in Warsaw was at 10AM in UTC)
+
+var dateFromShanghai = new DateFromTimezone("Asia/Shanghai");
+var shanghaiNoon = dateFromShanghai.getDate(2017, 6, 5, 12);
+console.log(shanghaiNoon.toISOString()); // "2017-07-05T04:00:00.000Z" (12PM in Shanghai was at 4AM in UTC)
+```
+
+### Installation
+
+	$ npm install date-from-timezone
+
+### Test
+
+Project cross-browser compatibility supported by:
+
+<a href="https://browserstack.com"><img src="https://bstacksupport.zendesk.com/attachments/token/Pj5uf2x5GU9BvWErqAr51Jh2R/?name=browserstack-logo-600x315.png" height="150" /></a>
---
+++ index.js
@@ -0,0 +1,111 @@
+"use strict";
+
+var copyDate     = require("es5-ext/date/#/copy")
+  , assign       = require("es5-ext/object/assign")
+  , ensureString = require("es5-ext/object/validate-stringifiable-value")
+  , d            = require("d");
+
+var refLocale = "en", dateStrRe = /^(\d{2})\/(\d{2})\/(\d{1,4}), (\d{2}):(\d{2}):(\d{2})$/;
+
+var formatOptions = {
+	hour12: false,
+	year: "numeric",
+	month: "2-digit",
+	day: "2-digit",
+	hour: "2-digit",
+	minute: "2-digit",
+	second: "2-digit"
+};
+
+// Detect support
+if (
+	typeof Intl !== "object" ||
+	!Intl ||
+	typeof Intl.DateTimeFormat !== "function" ||
+	typeof Intl.DateTimeFormat.supportedLocalesOf !== "function" ||
+	Intl.DateTimeFormat.supportedLocalesOf(refLocale)[0] !== refLocale ||
+	new Intl.DateTimeFormat(refLocale, assign({ timeZone: "Europe/Warsaw" }, formatOptions)).format(
+		new Date(1496316716561)
+	) !== "06/01/2017, 13:31:56" ||
+	new Intl.DateTimeFormat(refLocale, assign({ timeZone: "Asia/Shanghai" }, formatOptions)).format(
+		new Date(1496316716561)
+	) !== "06/01/2017, 19:31:56"
+) {
+	module.exports = null;
+	return;
+}
+
+// Localized date string tokenizer
+var tokenizeDateStr = function (str) {
+	var match = str.match(dateStrRe);
+
+	return {
+		year: Number(match[3]),
+		month: Number(match[1]) - 1,
+		date: Number(match[2]),
+		hours: Number(match[4]),
+		minutes: Number(match[5]),
+		seconds: Number(match[6])
+	};
+};
+
+var DateFromTimezone = module.exports = function (timezone) {
+	if (!(this instanceof DateFromTimezone)) {
+		throw new TypeError("Constructor DateFromTimezone requires 'new'");
+	}
+
+	this.timezone = ensureString(timezone);
+
+	// Validate support for timezone, let eventual error propagate freely
+	// eslint-disable-next-line no-new
+	new Intl.DateTimeFormat(refLocale, { timeZone: this.timezone });
+
+	this._formatOptions = assign({ timeZone: this.timezone }, formatOptions);
+};
+
+var yearZero = new Date(-1, 12, 2);
+
+Object.defineProperties(DateFromTimezone.prototype, {
+	getDate: d(function (year, month /*, date, hours, minutes, seconds, milliseconds*/) {
+		if (isNaN(year) || isNaN(month)) {
+			throw new TypeError(
+				"Invalid getDate arguments: Expected: " +
+					"year, month[, date[, hours[, minutes[, second[, milliseconds]]]]]"
+			);
+		}
+		var refDate = new Date(
+			year,
+			month,
+			arguments[2] || 1,
+			arguments[3] || 0,
+			arguments[4] || 0,
+			arguments[5] || 0,
+			arguments[6] || 0
+		);
+
+		if (isNaN(refDate)) {
+			throw new TypeError("Invalid getDate arguments: Invalid date data");
+		}
+		if (refDate < yearZero) {
+			throw new TypeError("Invalid getDate arguments: No support for BC years");
+		}
+		return this._resolveDate(refDate, copyDate.call(refDate), 3);
+	}),
+	_resolveDate: d(function (refDate, resultDate) {
+		var tokens = tokenizeDateStr(
+			new Intl.DateTimeFormat(refLocale, this._formatOptions).format(resultDate)
+		);
+		var tmpDate = new Date(
+			tokens.year,
+			tokens.month,
+			tokens.date,
+			tokens.hours,
+			tokens.minutes,
+			tokens.seconds
+		);
+
+		if (tmpDate.getTime() === refDate.getTime()) return resultDate;
+		resultDate.setTime(resultDate.getTime() + refDate.getTime() - tmpDate.getTime());
+		return this._resolveDate(refDate, resultDate);
+	})
+});
