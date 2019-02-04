cf21835d82b8cac0619c019b6e59fe822e7020e9
Mariusz Nowak <medyk@medikoo.com>
2019-02-04 21:14:58 +0100

    feat: convert to ES2015

--- index.js
+++ index.js
@@ -8,17 +8,15 @@ if (!getTokenize) {
 	return;
 }
 
-var resolveDate = function (refDate, resultDate, tokenize) {
+const resolveDate = function (refDate, resultDate, tokenize) {
 	const tokens = tokenize(resultDate);
 	const resolvedDate = new Date(
-		tokens.year, tokens.month, tokens.date, tokens.hours, tokens.minutes,
-		tokens.seconds, tokens.milliseconds
+		tokens.year, tokens.month, tokens.date, tokens.hours, tokens.minutes, tokens.seconds,
+		tokens.milliseconds
 	);
 
 	if (resolvedDate.getTime() === refDate.getTime()) return resultDate;
-	resultDate.setTime(
-		resultDate.getTime() + refDate.getTime() - resolvedDate.getTime()
-	);
+	resultDate.setTime(resultDate.getTime() + refDate.getTime() - resolvedDate.getTime());
 	return resolveDate(refDate, resultDate, tokenize);
 };
 
@@ -26,20 +24,14 @@ module.exports = function (timezone) {
 	const tokenize = getTokenize(timezone);
 
 	// eslint-disable-next-line no-unused-vars
-	return function (
-		year,
-		month /* , date, hours, minutes, seconds, milliseconds*/
-	) {
+	return function (year, month, date = 1, hours = 0, minutes = 0, seconds = 0, milliseconds = 0) {
 		if (isNaN(year) || isNaN(month)) {
 			throw new TypeError(
 				"Invalid arguments: Expected: " +
 					"year, month[, date[, hours[, minutes[, second[, milliseconds]]]]]"
 			);
 		}
-		const date = new Date(
-			year, month, arguments[2] || 1, arguments[3] || 0, arguments[4] || 0,
-			arguments[5] || 0, arguments[6] || 0
-		);
-		return resolveDate(date, copyDate.call(date), tokenize);
+		const sampleDate = new Date(year, month, date, hours, minutes, seconds, milliseconds);
+		return resolveDate(sampleDate, copyDate.call(sampleDate), tokenize);
 	};
 };
